258b6f153639e381948fbba422cd0a4982b9b568
Lilg <lilg@chocwell.com>
2023-02-24 01:12:53 +0800

    init

---
+++ src/main/java/org/sky/DemoMain8970.java
@@ -0,0 +1,15 @@
+package org.sky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *@author lilg
+ *@date 2023/2/24
+ */
+@SpringBootApplication
+public class DemoMain8970 {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMain8970.class, args);
+	}
+}
---
+++ src/main/java/org/sky/scheduled/XxlJobDemo.java
@@ -0,0 +1,8 @@
+package org.sky.scheduled;
+
+/**
+ *@author lilg
+ *@date 2023/2/24
+ */
+public class XxlJobDemo {
+}
